1ab6d5b9edec6e8ca1a0e75f633a21d993a0d966
WWDC <wwdc@kumars-macbook-pro.local>
2016-03-10 21:12:49 +0530

    This is shown after doing the changes

--- stack1/ViewController.m
+++ stack1/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // this revision is shown after doing the changes.
     // Do any additional setup after loading the view, typically from a nib.
 }
 
